e30abcd6a2c46de80b7b7aaf7245587933e46f3b
Lei S <ukeleileis@gmail.com>
2020-02-28 23:52:50 -0800

    Changes

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+
+    mars_dict_data = mongo.db.data.find_one()
+    return render_template("index.html", mars_index=mars_dict_data)
+
+
+@app.route("/scrape")
+def scrape():
+
+    mars_dict = scrape_mars.scrape()
+
+    mongo.db.data.update({}, mars_dict, upsert=True)
+
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import requests
+import pandas as pd
+
+def scrape():
+  executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+  browser = Browser('chrome', **executable_path, headless=False)
+
+
+  #1 Mars NASA Latest News
+  new_mars = 'https://mars.nasa.gov/news/'
+  browser.visit(new_mars)
+  html = browser.html
+  soup = bs(html, 'html.parser')
+
+  news_t = soup.find('div', class_="content_title").text
+
+  # JPL Mars Space Images
+  featured_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+  browser.visit(featured_url)
+  html_img = browser.html
+  img_soup = bs(html_img, 'html.parser')
+
+  img_soup_img = img_soup.find(class_='carousel_item')["style"]
+  base_img_url = 'https://www.jpl.nasa.gov'
+  featured_image_url = base_img_url + img_soup_img.strip('background-image: url').strip(";(')")
+
+
+  # Mars Twitter Weather
+  m_tweets = 'https://twitter.com/marswxreport?lang=en'
+  m_response = requests.get(m_tweets).text 
+  mars_tweets_soup = bs(m_response, 'html.parser')
+
+  mars_weather = mars_tweets_soup.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text.strip('pic.twitter.com/ihFGVkib6L')
+
+  # Mars Fact Table
+  mars_facts_url = 'https://space-facts.com/mars/'
+  tables = pd.read_html(mars_facts_url)
+
+  mars_table_facts = tables[0]
+  mars_table_facts = mars_table_facts.rename(columns={0: "Description", 1: "Value"}).set_index("Description")
+  mars_html = mars_table_facts.to_html(index=False).replace('\n', '')
+
+
+  # Mars Hemispheres
+  hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+  hem_base_url = 'https://astrogeology.usgs.gov'
+  browser.visit(hem_url)
+  hem_html = browser.html
+  hem_soup = bs(hem_html, 'html.parser')
+  hem_links = hem_soup.find_all('div', class_='item')
+
+  hemisphere_image_urls = []
+
+  for links in hem_links:
+      
+      #Links
+      #Concat links for image pages
+      img_urls = hem_base_url + links.find("a", class_="itemLink product-item")['href']
+      #Soup again
+      browser.visit(img_urls)
+      img_urls_html = browser.html
+      loop_soup = bs(img_urls_html, 'html.parser')
+      #Finding the links for the FULL DOWNLOAD image
+      loop_links = loop_soup.find("img", class_="wide-image")['src']
+      loopy_links = hem_base_url + loop_links
+      
+      
+      #Title
+      title = links.find("h3").text
+      
+      #Append
+      hemisphere_image_urls.append({"title":title, "img_url": loopy_links})
+  
+  # End
+  browser.quit()
+
+
+  # Dictionary
+  mars_dict = {
+    "news_t": news_t,
+    "featured_image_url": featured_image_url,
+    "mars_weather": mars_weather,
+    "table_html": mars_html,
+    "hemisphere_image_urls": hemisphere_image_urls
+}
+
+  return mars_dict
+
+
